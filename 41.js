00e617708ce44c4ede47a8bcc6d3e4dfa9ec9512
nesuesu <viktor.lavrinovic@gmail.com>
2023-01-04 23:33:48 +0200

    kortelės update

--- 41.js
+++ 41.js
@@ -9,14 +9,56 @@ fetch("korteles.json")
         let card = document.createElement("div");
         card.classList.add("card")
 
+
+        let image_div = document.createElement("div");
+        // image_div.style.position = relative;
         let image = document.createElement("img");
         image.setAttribute("src", element.image);
+        // image.style.position = "relative";
+        let p0 = document.createElement("p");
+        let tekstas0 = document.createTextNode("Duration: " + element.trukme + " hours");
+        p0.append(tekstas0);
+        p0.classList.add("ago");
 
-        let title = document.createElement("h1");
+        image_div.append(image,p0);
+
+
+
+        let title_div = document.createElement("div");
+        title_div.style.display = "flex";
+        title_div.style.alignItems = "center";
+
+        // title_div.style.marginTop = "20px";
+        let ikona = document.createElement("img");
+        ikona.classList.add("ikona");
+        ikona.setAttribute("src", element.ikona);
+        let title = document.createElement("h2");
         let tekstas = document.createTextNode(element.pavadinimas);
         title.append(tekstas);
+        title_div.append(ikona,title);
 
-        card.append(image,title);
+
+
+        let info = document.createElement("div");
+        info.style.marginBottom = "20px";
+        info.style.marginLeft = "20px";
+
+        let channel = document.createElement("span");
+        let tekstas1 = document.createTextNode("Channel:" + element.channel + " ");
+        channel.append(tekstas1);
+
+        let views = document.createElement("span");
+        let tekstas2 = document.createTextNode("Views:" + element.views + " ");
+        views.append(tekstas2);
+
+        let ago = document.createElement("span");
+        let tekstas3 = document.createTextNode("Uploaded:" + element.ago + " hours ago");
+        ago.append(tekstas3);
+
+        let br = document.createElement("br");
+        info.append(channel,br,views,ago);
+        
+        card.append(image_div,title_div,info);
         page.append(card);
 
     });
